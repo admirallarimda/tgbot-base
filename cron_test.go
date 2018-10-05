aae90d2e0241c98e5d79a66bdffbcfe55a6c2f81
Ilya Lavrinov <ilya.v.lavrinov@gmail.com>
2018-10-05 23:19:34 +0300

    Avoiding race detector in tests

--- cron_test.go
+++ cron_test.go
@@ -3,14 +3,15 @@ package botbase
 import "testing"
 import "time"
 import "math/rand"
+import "sync/atomic"
 
 type testCronCountingJob struct {
-	count  int
+	count  int32
 	repeat *time.Duration
 }
 
 func (j *testCronCountingJob) Do(t time.Time, c Cron) {
-	j.count++
+	atomic.AddInt32(&j.count, 1) // atomic to avoid race detector
 	if j.repeat != nil {
 		c.AddJob(t.Add(*j.repeat), j)
 	}
@@ -21,6 +22,7 @@ func TestCallOnce(t *testing.T) {
 	j := &testCronCountingJob{}
 	c.AddJob(time.Now(), j)
 	time.Sleep(time.Second)
+	atomic.LoadInt32(&j.count)
 	if j.count != 1 {
 		t.Fatal(j.count)
 	}
@@ -31,12 +33,14 @@ func TestCallXTimes(t *testing.T) {
 	j := &testCronCountingJob{}
 
 	now := time.Now()
-	n := 5 + rand.Intn(5)
-	for i := 0; i < n; i++ {
+	n := 5 + rand.Int31n(5)
+	var i int32
+	for ; i < n; i++ {
 		c.AddJob(now, j)
 	}
 
 	time.Sleep(time.Second)
+	atomic.LoadInt32(&j.count)
 	if j.count != n {
 		t.Fatal(j.count, n)
 	}
